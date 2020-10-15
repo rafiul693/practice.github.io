069724511273f92cf2521dc6e840b9b078ba88f4
rafiul693 <72083539+rafiul693@users.noreply.github.com>
2020-10-15 21:28:20 +0600

    For Practice

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+		<p>&copy;2020 All Rights Reserved</p>
+	</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Contact Form</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Registration Form</h2>
+	</div>
